cdac1ee3f2c0a27230a599e5c1e1b7dead4051c2
Kristoffer Karlsson <kristoffer124@gmail.com>
2022-11-29 11:29:38 +0100

    Fixed function naming of logging methods

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,5 +4,12 @@ namespace Webbhuset\CollectorCheckout\Logger;
 
 class Logger extends \Magento\Framework\Logger\Monolog
 {
-
+    public function addCritical($message, $context)
+    {
+        parent::critical($message, $context);
+    }
+    public function addInfo($message, $context)
+    {
+        parent::info($message, $context);
+    }
 }
