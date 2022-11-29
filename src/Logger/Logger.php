d4adc247cc41fb105d45af79fd66cc473d76774b
Kristoffer Karlsson <kristoffer124@gmail.com>
2022-11-29 12:24:56 +0100

    Made additional parameters optional in logger

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,11 +4,11 @@ namespace Webbhuset\CollectorCheckout\Logger;
 
 class Logger extends \Magento\Framework\Logger\Monolog
 {
-    public function addCritical($message, $context)
+    public function addCritical($message, $context = [])
     {
         parent::critical($message, $context);
     }
-    public function addInfo($message, $context)
+    public function addInfo($message, $context = [])
     {
         parent::info($message, $context);
     }
