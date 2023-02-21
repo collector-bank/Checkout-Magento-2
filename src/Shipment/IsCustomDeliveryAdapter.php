a7e8b76d6ed9a857d1f170587c48e2a693a8b1d4
Kristoffer Karlsson <kristoffer124@gmail.com>
2023-02-21 09:37:56 +0100

    Added delivery cost to validation for delivery adapter

---
+++ src/Shipment/IsCustomDeliveryAdapter.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Webbhuset\CollectorCheckout\Shipment;
+
+class IsCustomDeliveryAdapter
+{
+    public function execute(
+        \Webbhuset\CollectorCheckoutSDK\CheckoutData $checkoutData
+    ):bool {
+        $shipment = $checkoutData->getShipping();
+        if (!$shipment) {
+            return false;
+        }
+        $shipmentData = $shipment->getData();
+        if (
+            isset($shipmentData["shipments"][0]["id"])
+            && $shipmentData["shipments"][0]["id"] === 'magento-delivery-methods'
+            && isset($shipmentData["shipments"][0]['shippingChoice']['id'])
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    public function getDeliveryFee(\Webbhuset\CollectorCheckoutSDK\CheckoutData $checkoutData):?float
+    {
+        if (!$this->execute($checkoutData)) {
+            return null;
+        }
+        $shipment = $checkoutData->getShipping()->getData();
+
+        return (float) $shipment["shipments"][0]['shippingChoice']['fee'];
+    }
+
+    public function getDeliveryMethod(\Webbhuset\CollectorCheckoutSDK\CheckoutData $checkoutData):?string
+    {
+        if (!$this->execute($checkoutData)) {
+            return null;
+        }
+        $shipment = $checkoutData->getShipping()->getData();
+
+        return (string) $shipment["shipments"][0]['shippingChoice']['id'];
+    }
+}
