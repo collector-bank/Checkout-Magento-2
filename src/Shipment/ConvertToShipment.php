7881140f69bf605cf2cda09c525bb569a936bb1d
Kristoffer Karlsson <kristoffer124@gmail.com>
2023-07-06 10:22:32 +0200

    Sort order for delivery methods

--- src/Shipment/ConvertToShipment.php
+++ src/Shipment/ConvertToShipment.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Webbhuset\CollectorCheckout\Shipment;
 
 use Magento\Quote\Api\Data\ShippingMethodInterface;
+use Magento\Shipping\Helper\Carrier;
 
 class ConvertToShipment
 {
@@ -11,11 +12,17 @@ class ConvertToShipment
      * @var GetIconForShippingMethod
      */
     private $getIconForShippingMethod;
+    /**
+     * @var Carrier
+     */
+    private $carrier;
 
     public function __construct(
-        GetIconForShippingMethod $getIconForShippingMethod
+        GetIconForShippingMethod $getIconForShippingMethod,
+        Carrier $carrier
     ) {
         $this->getIconForShippingMethod = $getIconForShippingMethod;
+        $this->carrier = $carrier;
     }
 
     /**
@@ -30,6 +37,8 @@ class ConvertToShipment
         foreach ($shippingMethods as $shippingMethod) {
             $shippingChoices[] = $this->shippingMethodToShipmentChoice($shippingMethod);
         }
+        $sortOrder = array_column($shippingChoices, 'sort_order');
+        array_multisort($sortOrder, SORT_ASC, $shippingChoices);
 
         return [
             'shipments' => [
@@ -54,6 +63,8 @@ class ConvertToShipment
             'fee' => (float)$shippingMethod->getPriceInclTax(),
             'icon' => $this->getIconForShippingMethod->execute($shippingMethod->getMethodCode()),
             'destinations' => null,
+            'sort_order' => (int) $this->carrier->getCarrierConfigValue($shippingMethod->getCarrierCode(), 'sort_order'),
         ];
     }
+
 }
