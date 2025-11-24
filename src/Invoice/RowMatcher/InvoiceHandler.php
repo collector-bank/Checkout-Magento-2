7caf10179a3a9d8732543192d7cb2e1806cc40d0
Kristoffer Karlsson <kristoffer124@gmail.com>
2025-11-24 22:14:52 +0100

    Refactor activation and creditation

--- src/Invoice/RowMatcher/InvoiceHandler.php
+++ src/Invoice/RowMatcher/InvoiceHandler.php
@@ -22,91 +22,17 @@ class InvoiceHandler
     protected $orderRepository;
 
     /**
-     * @var \Webbhuset\CollectorCheckout\Helper\Translation
-     */
-    protected $translation;
-    /**
-     * @var \Webbhuset\CollectorCheckout\Helper\GetSkuSuffix
-     */
-    protected $getSkuSuffix;
-
-    /**
-     * @var ProductType
-     */
-    protected $productType;
-
-    /**
      * rowMatcher constructor.
      */
     public function __construct(
         \Webbhuset\CollectorCheckout\Data\OrderHandler $orderHandler,
         \Webbhuset\CollectorCheckout\Adapter $adapter,
         \Magento\Sales\Api\OrderItemRepositoryInterface $orderItemRepository,
-        \Webbhuset\CollectorCheckout\Helper\Translation $translation,
-        ProductType $productType,
-        \Webbhuset\CollectorCheckout\Helper\GetSkuSuffix $getSkuSuffix,
         \Magento\Sales\Model\OrderRepository $orderRepository
     ) {
         $this->orderHandler         = $orderHandler;
         $this->orderItemRepository  = $orderItemRepository;
-        $this->translation          = $translation;
-        $this->getSkuSuffix         = $getSkuSuffix;
-        $this->productType          = $productType;
         $this->orderRepository      = $orderRepository;
-    }
-
-    /**
-     *
-     * Add items and discount as matchingArticles
-     *
-     * @param ArticleList                            $matchingArticles
-     * @param ArticleList                            $articleList
-     * @param \Magento\Sales\Model\Order\Creditmemo  $creditMemo
-     * @param \Magento\Sales\Api\Data\OrderInterface $order
-     * @return ArticleList
-     */
-    public function addItemsAndDiscounts(
-        ArticleList $matchingArticles,
-        ArticleList $articleList,
-        \Magento\Sales\Model\Order\Invoice $invoice,
-        \Magento\Sales\Api\Data\OrderInterface $order
-    ): ArticleList {
-        foreach ($invoice->getAllItems() as $invoiceItem) {
-            $productType = $this->productType->getProductTypeById((int)$invoiceItem->getProductId());
-            $sku = $invoiceItem->getSku();
-
-            if ($invoiceItem->getQty() > 0) {
-                if ($productType === \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE) {
-                    $skuSuffix = $this->getSkuSuffix->execute($invoiceItem->getSku());
-                    if ($skuSuffix) {
-                        $sku = $sku . $skuSuffix;
-                    }
-                }
-                if ($invoiceItem->getPrice() > 0) {
-                    $article = $articleList->getArticleBySku($sku);
-                } else {
-                    $article = $articleList->getArticleBySku("- " . $invoiceItem->getSku());
-                }
-
-                if ($article) {
-                    $article->setQuantity($invoiceItem->getQty());
-                    $matchingArticles->addArticle($article);
-
-                    if ($productType !== \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE) {
-                        $discountArticle = $articleList->getDiscountArticleBySku($invoiceItem->getSku() . "-1");
-                    } else {
-                        $discountLabel = $this->translation->getLabelByStoreId("Discount", $order->getStoreId());
-                        $discountArticle = $articleList->getDiscountArticleBySku($discountLabel . ": " . $sku);
-                    }
-
-                    if ($discountArticle) {
-                        $discountArticle->setQuantity($invoiceItem->getQty());
-                        $matchingArticles->addArticle($discountArticle);
-                    }
-                }
-            }
-        }
-        return $matchingArticles;
     }
 
     /**
@@ -185,18 +111,5 @@ class InvoiceHandler
     ):void {
         $this->orderHandler->setDecimalRoundingInvoiced($order);
         $this->orderRepository->save($order);
-    }
-
-    /**
-     * Get item from order from quote item id
-     *
-     * @param int $orderItemId
-     * @return int|null
-     */
-    public function getItemQuoteIdBy(int $orderItemId)
-    {
-        $orderItem = $this->orderItemRepository->get($orderItemId);
-
-        return $orderItem->getQuoteItemId();
     }
 }
